8ca2f69e3746330cd29a373838bba873b4394bf1
saksham704 <sb031428@gmail.com>
2025-09-13 12:27:10 +0530

    Add files via upload

---
+++ MiniMumInARotatedSortedArray.py
@@ -0,0 +1,13 @@
+def find_min(nums):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        else:
+            right = mid
+    return nums[left]
+
+
+arr = [4,5,6,7,0,1,2]
+print(find_min(arr))  # Output: 0
